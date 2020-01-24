a5baa8548432b97cc50b4700b87c66149cde083d
Abner A Oliveira <abneralves10@hotmail.com>
2020-01-24 13:43:47 -0300

    Changed type notes in the database

--- database/migrations/2020_01_23_211413_create_notes_table.php
+++ database/migrations/2020_01_23_211413_create_notes_table.php
@@ -18,7 +18,7 @@ class CreateNotesTable extends Migration
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->string('title');
-            $table->string('note');
+            $table->longText('note');
             $table->timestamps();
         });
     }
