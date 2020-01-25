78f7bce786814763f9ccd772c60d1d83484e42cb
Abner A Oliveira <abneralves10@hotmail.com>
2020-01-25 13:05:49 -0300

    Format date

--- app/Note.php
+++ app/Note.php
@@ -3,15 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Note extends Model
 {
     protected $fillable = [
         'title', 'note', 'category_id'
     ];
+    protected $casts = [
+        'created_at' => 'datetime:d/m/Y',
+        'updated_at' => 'datetime:d/m/Y',
+    ];
 
     public function category()
     {
-        return $this->belongsTo('App/Category');
+        return $this->belongsTo('App\Category');
     }
 }
